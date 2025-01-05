1473aab95286633d3884e851250c57ef85d849fe
denisx555 <denis.nugumanov@gmail.com>
2025-01-05 17:09:24 +0300

    Context menu added

--- src/NuGetToProjectReferenceConverter/NuGetToProjectReferenceConverterPackage.cs
+++ src/NuGetToProjectReferenceConverter/NuGetToProjectReferenceConverterPackage.cs
@@ -25,6 +25,7 @@ namespace NuGetToProjectReferenceConverter
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(NuGetToProjectReferenceConverterPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class NuGetToProjectReferenceConverterPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace NuGetToProjectReferenceConverter
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await Command.InitializeAsync(this);
         }
 
         #endregion
